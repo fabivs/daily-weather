21df64b57be3598abf45faf31e700705eb13039a
fabivs <fabio.prizio@casavo.com>
2023-10-08 19:17:46 +0200

    test: add weather service test

--- test/daily_weather/services/weather_test.exs
+++ test/daily_weather/services/weather_test.exs
@@ -1,9 +1,26 @@
 defmodule DailyWeather.Services.WeatherTest do
   use ExUnit.Case
 
-  # Probably will have to just mock the OpenMeteo module...
+  alias DailyWeather.Services.Weather
+  alias DailyWeather.Support.Fixtures
+  alias DailyWeather.Services.Clients.OpenMeteo
+  alias DailyWeather.Location
 
-  test "wip" do
-    :ok
+  import Fixtures
+  import Mock
+
+  test "Get a WeatherReport" do
+    # TODO: I should setup a custom Date module to be able to mock it in tests
+    date = Date.utc_today()
+
+    location = %Location{
+      lat: 50,
+      lon: 10
+    }
+
+    with_mock OpenMeteo,
+      get_weather_for: fn ^location, ^date, ^date -> forecast_successful_response_fixture() end do
+      assert Weather.weather_report_for_today(location) == {:ok, {14.8, 25.6}}
+    end
   end
 end
